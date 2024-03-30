fc3a6e33534be6dcc85a234a6c3c1ee57d8af9a8
Shubham2157 <1830193@kiit.ac.in>
2024-03-30 17:19:41 +0530

    fix apis exception

--- index.js
+++ index.js
@@ -21,32 +21,42 @@ const corsOptions = {
  app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
+    console.log(process.env.FRONTEND_URL);
   res.send("Server Running");
 });
 
 app.get("/api/products", (req, res) => {
-  // url = http://localhost:1337/api/products?populate=*
   axios
     .get(`${process.env.API_BASE_URL}/api/products?populate=*`)
     .then((response) => {
       // Handle successful response
-      // console.log('Response data:', response.data);
+      // console.log('Response data:', JSON.stringify(response.data));
 
-      const filteredData = response.data.data.map((item) => ({
-        id: item.id,
-        name: item.attributes.name,
-        description: item.attributes.description,
-        imageUrl: item.attributes.image.data[0].attributes.url,
-        imageUrls: item.attributes.image.data.map(
-          (image) => image.attributes.url
-        ),
-      }));
+        const filteredData = response.data.data.map((item) => {
+        const imageUrl = item.attributes.image && item.attributes.image.data ?
+          (item.attributes.image.data[0] ? item.attributes.image.data[0].attributes.url : null) : null;
+      
+        return {
+          id: item.id,
+          name: item.attributes.name,
+          description: item.attributes.description,
+          imageUrl: imageUrl,
+          price: item.attributes.price,
+          info: item.attributes.info,
+          slug: item.attributes.slug,
+          imageUrls: item.attributes.image && item.attributes.image.data ?
+            item.attributes.image.data.map((image) => image.attributes.url) : [],
+        };
+      });
+      
+
+
       res.send(filteredData);
     })
     .catch((error) => {
       // Handle error
       // console.error('Error:', error.message);
-      res.send(error.message);
+      res.send(error);
     });
 });
 
@@ -62,19 +72,25 @@ app.get("/api/product/:id", (req, res) => {
         id: response.data.data.id,
         name: response.data.data.attributes.name,
         description: response.data.data.attributes.description,
-        imageUrl: response.data.data.attributes.image.data[0].attributes.url,
-        imageUrls: response.data.data.attributes.image.data.map(
-          (image) => image.attributes.url
-        ),
+        price: response.data.data.attributes.price,
+        info: response.data.data.attributes.info,
+        slug: response.data.data.attributes.slug,
+        imageUrl: response.data.data.attributes.image && response.data.data.attributes.image.data && response.data.data.attributes.image.data[0] ?
+          response.data.data.attributes.image.data[0].attributes.url : null,
+        imageUrls: response.data.data.attributes.image && response.data.data.attributes.image.data ?
+          response.data.data.attributes.image.data.map((image) => image.attributes.url) : [],
       };
+      
       res.send(filteredData);
     })
     .catch((error) => {
       // Handle error
       // console.error('Error:', error.message);
-      res.send(error.message);
+      res.send(error);
     });
 });
+
+http://localhost:1337/api/products?filters[slug]=product-1&populate=*
 
 app.listen(PORT, () => {
   console.log(`Server started on PORT : ${PORT}`);
